d8c711c73e772ebea4ed605c0b2dfd9848c44d3e
sergeiivankov <sergeiivankov@protonmail.com>
2022-03-20 01:42:52 +0300

    Fix pack task web and safari

--- build/gulp/pack.js
+++ build/gulp/pack.js
@@ -4,14 +4,16 @@ const zip = require('gulp-zip');
 const { TARGETS, TARGETS_BUILD_DESTS } = require('./config');
 const { version } = require('../../package.json');
 
-const pack = targetName => {
-    if (targetName === TARGETS.SAFARI) {
-        return new Promise((resolve, reject) => {
-            if (process.platform !== 'darwin') {
-                console.log('Pack target "safari" available only on MacOS');
-                return resolve();
-            }
+const pack = async targetName => {
+    if (targetName === TARGETS.WEB) return;
 
+    if (targetName === TARGETS.SAFARI) {
+        if (process.platform !== 'darwin') {
+            console.log('Pack target "safari" available only on MacOS');
+            return;
+        }
+
+        return new Promise((resolve, reject) => {
             const child = spawn(
                 'xcodebuild', ['-project', 'build/safari/TON Wallet.xcodeproj'],
                 { stdio: 'inherit' }
